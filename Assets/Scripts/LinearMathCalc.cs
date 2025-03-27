03780f350f472eed7a8bb9284f236006d144d9fe
Michael Do <dotansang@live.com>
2025-03-27 16:18:43 -0400

    new file:   Assets/Scripts/LinearMathCalc.cs 	new file:   Assets/Scripts/LinearMathCalc.cs.meta 	modified:   Packages/manifest.json 	modified:   Packages/packages-lock.json 	modified:   ProjectSettings/ProjectVersion.txt

---
+++ Assets/Scripts/LinearMathCalc.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LinearMathCalc
+{
+    public string Input { get; set; }
+    public float Output { get; }
+    public LinearMathCalc(string input)
+    {
+        this.Input = input;
+        this.Output = 0;
+    }
+    public float Calc(string input)
+    {
+        
+        return 0;
+    }
+}
